dc745b64a29bff21d44094e300eea21f447e02d6
lee-junghyun <momneka23@gmail.com>
2022-05-02 15:19:02 +0900

    테스트 함수

---
+++ library/src/main/java/com/junghyun/library/TimeManager.kt
@@ -0,0 +1,44 @@
+package com.junghyun.library
+
+import java.text.SimpleDateFormat
+
+public class TimeManager {
+
+    var mTimeFormat: String = "yyyy-mm-dd hh:mm:ss"
+//    var mCountryCode: Int = 0
+//
+//    constructor(timeFormat: String) {
+//
+//        this.mTimeFormat = timeFormat
+//
+//    }
+//
+//    constructor(timeFormat: String, countryCode: Int) {
+//
+//        this.mTimeFormat = timeFormat
+//        this.mCountryCode = countryCode
+//
+//    }
+
+    fun getTime(): String?{
+        return timeGetter(mTimeFormat)
+    }
+
+    fun getTime(timeFormat: String, countryCode: Int){
+
+    }
+
+    fun getTime(countryCode: Int){
+
+
+    }
+
+    fun timeGetter(timeFormat: String): String? {
+        val currentTime : Long = System.currentTimeMillis()
+        val dataFormat = SimpleDateFormat(timeFormat)
+        return dataFormat.format(currentTime)
+    }
+
+
+
+}
