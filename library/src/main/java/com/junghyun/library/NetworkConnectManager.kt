e20059372d41362f9beec04bf9e1e4b9bfc418de
lee-junghyun <momneka23@gmail.com>
2022-05-10 17:25:38 +0900

    네트워크 콜백 해제부분 오류 수정

--- library/src/main/java/com/junghyun/library/NetworkConnectManager.kt
+++ library/src/main/java/com/junghyun/library/NetworkConnectManager.kt
@@ -34,7 +34,7 @@ class NetworkConnectManager(private val context: Context) : LiveData<Boolean>() 
     override fun onInactive() {
         super.onInactive()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            connectivityManager.unregisterNetworkCallback(connectivityManagerCallback())
+            connectivityManager.unregisterNetworkCallback(networkCallback)
         } else {
             context.unregisterReceiver(networkReceiver)
         }
